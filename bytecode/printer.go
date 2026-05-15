d812b7911780778d9baab44a4394a32160251e70
shinya <kobayashi_shinya@outlook.com>
2026-05-15 20:27:23 +0800

    fix(bytecode): print complete module state

--- bytecode/printer.go
+++ bytecode/printer.go
@@ -10,24 +10,28 @@ func PrintModule(m *Module) string {
 		return "<nil module>\n"
 	}
 	var b strings.Builder
-	fmt.Fprintf(&b, "Module target=%q endian=%s ptr=%d\n", m.Target.Name, m.Target.Endian, m.Target.PointerSize)
+	t := m.Target
+	fmt.Fprintf(&b, "Module target=%q endian=%s ptr_size=%d ptr_align=%d bool_size=%d bool_align=%d bitfield_policy=%q layout_version=%q\n",
+		t.Name, t.Endian, t.PointerSize, t.PointerAlign, t.BoolSize, t.BoolAlign, t.BitFieldPolicy, t.LayoutVersion)
 	for _, g := range m.Globals {
 		printGlobal(&b, g)
 	}
 	for _, s := range m.Strings {
-		fmt.Fprintf(&b, "String #%d value=%q size=%d\n", s.ID, s.Value, len(s.Bytes))
+		fmt.Fprintf(&b, "String #%d value=%q bytes=%d hex=%x\n", s.ID, s.Value, len(s.Bytes), s.Bytes)
 	}
 	for _, l := range m.Layouts {
-		fmt.Fprintf(&b, "Layout #%d name=%q size=%d align=%d\n", l.ID, l.Name, l.Size, l.Align)
+		fmt.Fprintf(&b, "Layout #%d name=%q size=%d align=%d elem_size=%d\n", l.ID, l.Name, l.Size, l.Align, l.ElemSize)
+		for _, f := range l.Fields {
+			fmt.Fprintf(&b, "  Field #%d name=%q offset=%d type=%s\n", f.ID, f.Name, f.Offset, f.Type)
+		}
+		for _, bf := range l.Bit {
+			fmt.Fprintf(&b, "  BitField #%d name=%q container=%s byte_offset=%d bit_offset=%d width=%d signed=%v volatile=%v policy=%q\n",
+				bf.ID, bf.Name, bf.Container, bf.ByteOffset, bf.BitOffset, bf.Width, bf.Signed, bf.Volatile, bf.LayoutPolicy)
+		}
 	}
 	for _, sig := range m.Sigs {
 		fmt.Fprintf(&b, "Sig #%d ret=%s params=(", sig.ID, sig.Ret)
-		for i, p := range sig.Params {
-			if i > 0 {
-				b.WriteString(", ")
-			}
-			b.WriteString(p.String())
-		}
+		writeValueTypes(&b, sig.Params)
 		if sig.Variadic {
 			if len(sig.Params) > 0 {
 				b.WriteString(", ")
@@ -49,17 +53,35 @@ func printGlobal(b *strings.Builder, g Global) {
 	case GlobalExtern:
 		fmt.Fprintf(b, "Global #%d extern name=%q\n", g.ID, g.Name)
 	default:
-		fmt.Fprintf(b, "Global #%d var name=%q size=%d align=%d readonly=%v\n", g.ID, g.Name, g.Size, g.Align, g.Readonly)
+		fmt.Fprintf(b, "Global #%d var name=%q size=%d align=%d readonly=%v init_zero=%d init_bytes=%d init_relocs=%d\n",
+			g.ID, g.Name, g.Size, g.Align, g.Readonly, g.Init.ZeroFill, len(g.Init.Bytes), len(g.Init.Relocations))
+		if len(g.Init.Bytes) > 0 {
+			fmt.Fprintf(b, "  InitBytes hex=%x\n", g.Init.Bytes)
+		}
+		for i, r := range g.Init.Relocations {
+			fmt.Fprintf(b, "  Reloc #%d offset=%d kind=%s target=%d addend=%d\n", i, r.Offset, relocationKindName(r.Kind), r.Target, r.Addend)
+		}
 	}
 }
 
 func printFunction(b *strings.Builder, f Function) {
-	fmt.Fprintf(b, "Func #%d global=%d name=%q sig=%d\n", f.ID, f.GlobalID, f.Name, f.Sig)
+	fmt.Fprintf(b, "Func #%d global=%d name=%q sig=%d max_stack=%d\n", f.ID, f.GlobalID, f.Name, f.Sig, f.MaxStack)
+	for _, p := range f.Params {
+		fmt.Fprintf(b, "  Param slot=%d name=%q type=%s\n", p.Slot, p.Name, p.Type)
+	}
 	for _, l := range f.Locals {
 		fmt.Fprintf(b, "  Local #%d name=%q type=%s\n", l.ID, l.Name, l.Type)
 	}
 	for _, o := range f.Objects {
 		fmt.Fprintf(b, "  Object #%d name=%q size=%d align=%d layout=%d\n", o.ID, o.Name, o.Size, o.Align, o.Layout)
+	}
+	for _, o := range f.DynamicObjects {
+		fmt.Fprintf(b, "  DynamicObject #%d name=%q align=%d layout=%d\n", o.ID, o.Name, o.Align, o.Layout)
+	}
+	for _, l := range f.Labels {
+		fmt.Fprintf(b, "  Label #%d name=%q stack=(", l.ID, l.Name)
+		writeValueTypes(b, l.Stack)
+		fmt.Fprintf(b, ") statement=%v\n", l.Statement)
 	}
 	for pc, ins := range f.Instrs {
 		fmt.Fprintf(b, "  %04d: %s\n", pc, FormatInstr(ins))
@@ -73,20 +95,58 @@ func FormatInstr(i Instr) string {
 			return fmt.Sprintf("%sConst %v", instrTypePrefix(i.Type), i.Float)
 		}
 		return fmt.Sprintf("%sConst %d", instrTypePrefix(i.Type), i.Int)
+	case OpAddrString:
+		return fmt.Sprintf("AddrString %d", i.Int)
+	case OpAddrGlobal:
+		return fmt.Sprintf("AddrGlobal %d", i.Global)
+	case OpAddrFunc:
+		return fmt.Sprintf("AddrFunc %d", i.Func)
+	case OpLoadConst:
+		return fmt.Sprintf("%sLoadConst global=%d offset=%d", instrTypePrefix(i.Type), i.Global, i.Int)
+	case OpDup:
+		return "Dup"
+	case OpPop:
+		return "Pop"
+	case OpSwap:
+		return "Swap"
 	case OpLoadLocal:
 		return fmt.Sprintf("%sLoadLocal %d", instrTypePrefix(i.Type), i.Slot)
 	case OpStoreLocal:
 		return fmt.Sprintf("%sStoreLocal %d", instrTypePrefix(i.Type), i.Slot)
 	case OpAddrLocalObject:
 		return fmt.Sprintf("AddrLocalObject %d", i.Object)
-	case OpAddrGlobal:
-		return fmt.Sprintf("AddrGlobal %d", i.Global)
+	case OpAllocDynamicObject:
+		return fmt.Sprintf("AllocDynamicObject object=%d size=%d align=%d layout=%d", i.Object, i.Size, i.Align, i.Layout)
+	case OpFreeDynamicObject:
+		return fmt.Sprintf("FreeDynamicObject %d", i.Object)
+	case OpDynamicObjectAddr:
+		return fmt.Sprintf("DynamicObjectAddr %d", i.Object)
 	case OpLoad:
 		return fmt.Sprintf("%sLoad align=%d volatile=%v", instrTypePrefix(i.Type), i.Align, i.Volatile)
 	case OpStore:
 		return fmt.Sprintf("%sStore align=%d volatile=%v", instrTypePrefix(i.Type), i.Align, i.Volatile)
+	case OpMemCopy:
+		return fmt.Sprintf("MemCopy size=%d align=%d volatile=%v", i.Size, i.Align, i.Volatile)
+	case OpMemSet:
+		return fmt.Sprintf("MemSet size=%d align=%d volatile=%v", i.Size, i.Align, i.Volatile)
+	case OpOffset:
+		return fmt.Sprintf("%sOffset %d", instrTypePrefix(i.Type), i.Int)
+	case OpFieldAddr:
+		return fmt.Sprintf("FieldAddr layout=%d field=%d", i.Layout, i.Field)
+	case OpBitFieldLoad:
+		return fmt.Sprintf("%sBitFieldLoad layout=%d field=%d volatile=%v", instrTypePrefix(i.Type), i.Layout, i.Field, i.Volatile)
+	case OpBitFieldStore:
+		return fmt.Sprintf("%sBitFieldStore layout=%d field=%d volatile=%v", instrTypePrefix(i.Type), i.Layout, i.Field, i.Volatile)
+	case OpPtrAdd:
+		return fmt.Sprintf("%sPtrAdd elem_size=%d", instrTypePrefix(i.Type), i.Size)
+	case OpPtrDiff:
+		return fmt.Sprintf("%sPtrDiff elem_size=%d", instrTypePrefix(i.Type), i.Size)
 	case OpBinary:
 		return fmt.Sprintf("%s%s", instrTypePrefix(i.Type), binaryName(i.Binary))
+	case OpUnary:
+		return fmt.Sprintf("%s%s", instrTypePrefix(i.Type), unaryName(i.Unary))
+	case OpCast:
+		return fmt.Sprintf("Cast %s->%s %s", i.Type, i.Type2, castName(i.Cast))
 	case OpJump:
 		return fmt.Sprintf("Jump L%d", i.Label)
 	case OpJumpIfZero:
@@ -95,14 +155,28 @@ func FormatInstr(i Instr) string {
 		return fmt.Sprintf("JumpIfNonZero %s L%d", i.Type, i.Label)
 	case OpLabel:
 		return fmt.Sprintf("L%d:", i.Label)
+	case OpSwitch:
+		return fmt.Sprintf("Switch %s default=L%d cases=(%s)", i.Type, i.Label, switchCases(i.Labels))
 	case OpReturn:
 		return fmt.Sprintf("%sReturn", instrTypePrefix(i.Type))
 	case OpReturnVoid:
 		return "ReturnVoid"
+	case OpReturnObject:
+		return fmt.Sprintf("ReturnObject %d", i.Object)
+	case OpUnreachable:
+		return "Unreachable"
 	case OpCall:
 		return fmt.Sprintf("Call global=%d sig=%d argc=%d", i.Global, i.Sig, i.Argc)
+	case OpCallIndirect:
+		return fmt.Sprintf("CallIndirect sig=%d argc=%d", i.Sig, i.Argc)
+	case OpVaStart:
+		return fmt.Sprintf("VaStart slot=%d", i.Slot)
+	case OpVaArg:
+		return fmt.Sprintf("%sVaArg", instrTypePrefix(i.Type))
+	case OpVaEnd:
+		return fmt.Sprintf("VaEnd slot=%d", i.Slot)
 	default:
-		return fmt.Sprintf("Opcode(%d)", int(i.Op))
+		return fmt.Sprintf("InvalidOpcode(%d)", int(i.Op))
 	}
 }
 
@@ -153,6 +227,22 @@ func binaryName(op BinaryOp) string {
 		return "DivS"
 	case BinDivU:
 		return "DivU"
+	case BinRemS:
+		return "RemS"
+	case BinRemU:
+		return "RemU"
+	case BinAnd:
+		return "And"
+	case BinOr:
+		return "Or"
+	case BinXor:
+		return "Xor"
+	case BinShl:
+		return "Shl"
+	case BinShrS:
+		return "ShrS"
+	case BinShrU:
+		return "ShrU"
 	case BinEq:
 		return "Eq"
 	case BinNe:
@@ -161,17 +251,98 @@ func binaryName(op BinaryOp) string {
 		return "LtS"
 	case BinLtU:
 		return "LtU"
+	case BinLeS:
+		return "LeS"
+	case BinLeU:
+		return "LeU"
+	case BinGtS:
+		return "GtS"
+	case BinGtU:
+		return "GtU"
+	case BinGeS:
+		return "GeS"
+	case BinGeU:
+		return "GeU"
+	case BinLtF:
+		return "LtF"
+	case BinLeF:
+		return "LeF"
+	case BinGtF:
+		return "GtF"
+	case BinGeF:
+		return "GeF"
 	default:
-		return titleASCII(op.String())
+		return fmt.Sprintf("BinaryOp(%d)", int(op))
 	}
 }
 
-func titleASCII(s string) string {
-	if s == "" {
-		return ""
+func unaryName(op UnaryOp) string {
+	switch op {
+	case UnaryNeg:
+		return "Neg"
+	default:
+		return fmt.Sprintf("UnaryOp(%d)", int(op))
 	}
-	if s[0] >= 'a' && s[0] <= 'z' {
-		return string(s[0]-'a'+'A') + s[1:]
+}
+
+func castName(op CastOp) string {
+	switch op {
+	case CastTrunc:
+		return "Trunc"
+	case CastZExt:
+		return "ZExt"
+	case CastSExt:
+		return "SExt"
+	case CastFExt:
+		return "FExt"
+	case CastFTrunc:
+		return "FTrunc"
+	case CastIntToFloat:
+		return "IntToFloat"
+	case CastFloatToInt:
+		return "FloatToInt"
+	case CastPtrToInt:
+		return "PtrToInt"
+	case CastIntToPtr:
+		return "IntToPtr"
+	case CastBit:
+		return "Bit"
+	case CastBool:
+		return "Bool"
+	default:
+		return fmt.Sprintf("CastOp(%d)", int(op))
 	}
-	return s
+}
+
+func writeValueTypes(b *strings.Builder, ts []ValueType) {
+	for i, t := range ts {
+		if i > 0 {
+			b.WriteString(", ")
+		}
+		b.WriteString(t.String())
+	}
+}
+
+func relocationKindName(k RelocationKind) string {
+	switch k {
+	case RelocGlobal:
+		return "global"
+	case RelocFunc:
+		return "func"
+	case RelocString:
+		return "string"
+	default:
+		return fmt.Sprintf("reloc(%d)", int(k))
+	}
+}
+
+func switchCases(cases []SwitchCase) string {
+	var b strings.Builder
+	for i, c := range cases {
+		if i > 0 {
+			b.WriteString(", ")
+		}
+		fmt.Fprintf(&b, "%d:L%d", c.Value, c.Label)
+	}
+	return b.String()
 }
